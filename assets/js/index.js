09fbb89b7f06fb2fb440bcfcbc3c30dc4292ad94
zaidarivera15 <107581978+zaidarivera15@users.noreply.github.com>
2022-06-15 18:22:27 -0400

    karla fue
    se agrega la funcionalidad para el login

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,17 @@
-//location.href()
+
+function validar() {
+    var p = document.getElementById("typePasswordX").value;
+    var e = document.getElementById("typeEmailX").value;
+    var flag = true
+
+    usuarios.forEach(u => {
+      if (u.email == e && u.password == p) {
+        window.location.href = "productos.html"
+        flag= false
+      }
+    });
+    if (flag){
+     alert("correo o contraseña incorrecta") 
+    }
+    
+  }
